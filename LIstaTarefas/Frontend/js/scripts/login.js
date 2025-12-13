77ba9f250b8fac239fa32eeed36e06aed43cb1d7
Thiago Lopes <thiaguinlkk@gmail.com>
2025-12-13 16:25:39 -0300

    Delete LIstaTarefas directory

--- LIstaTarefas/Frontend/js/scripts/login.js
+++
@@ -1,9 +0,0 @@
-import { Login } from '../class/Login.js';
-
-const usuario = document.getElementById("usuario");
-const senha = document.getElementById("senha");
-const botao = document.getElementById("botao");
-const mensagem = document.getElementById("mensagem");
-
-const login = new Login(usuario, senha, botao, mensagem);
-login.autenticar();
