2c077bc4100fd109488a0bfe83f52adecca25add
Thiago Lopes <thiaguinlkk@gmail.com>
2025-12-13 16:23:07 -0300

    Delete js directory

--- js/scripts/home.js
+++
@@ -1,8 +0,0 @@
-const botaoNovo = document.getElementById("botaoNovo");
-
-// Para saber se o botão foi clicado
-this.botao.addEventListener("click", (event) => {
-
-    // Cria nova Div
-    const newDiv = document.createElement("div");
-});
